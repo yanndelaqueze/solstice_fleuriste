9529abcc7e1772ed0240e2a92aa8c5f5b4855e4e
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-10-11 17:54:17 +0200

    polygon drawing tool

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -9,6 +9,9 @@ eagerLoadControllersFrom("controllers", application);
 import TestController from "./test_controller";
 application.register("test", TestController);
 
+import PolygonDrawingController from "./polygon_drawing_controller";
+application.register("polygon-drawing", PolygonDrawingController);
+
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
