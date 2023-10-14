7a4c874a304f18fcbf355f2adeac0c4faad1c173
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-10-14 16:30:47 +0200

    small fixes

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -2,7 +2,7 @@ class ShopController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index]
 
   def index
-    @products = Product.all
-    @categories = Category.all
+    @products = Product.all.order(position: :asc)
+    @categories = Category.all.order(position: :asc)
   end
 end
