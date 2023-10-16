a5714567017a176b9eccc187a9a4d87129a7ffad
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-10-16 14:33:19 +0200

    image hover

--- app/javascript/controllers/image_hover_controller.js
+++ app/javascript/controllers/image_hover_controller.js
@@ -3,14 +3,16 @@ import { Controller } from "@hotwired/stimulus";
 // Connects to data-controller="image-hover"
 export default class extends Controller {
   static targets = ["image"];
+  static values = { secondImage: String };
 
   connect() {
     console.log("image hover hello");
+    console.log(this.secondImageValue);
   }
 
   showMouseEnter() {
     this.originalImage = this.imageTarget.src;
-    this.imageTarget.src = this.element.getAttribute("data-second-image");
+    this.imageTarget.src = this.secondImageValue;
   }
 
   showMouseLeave() {
