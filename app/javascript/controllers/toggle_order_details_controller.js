a61638dec662e369d2f8d28a1a85984e47c53b75
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-10-17 15:55:54 +0200

    order index - toggle details

--- app/javascript/controllers/toggle_order_details_controller.js
+++ app/javascript/controllers/toggle_order_details_controller.js
@@ -1,7 +1,29 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="toggle-order-details"
 export default class extends Controller {
+  static targets = ["details", "button"];
+
   connect() {
+    console.log("Hello Toggle Order Details");
+    this.hideDetails();
+  }
+
+  toggleDetails() {
+    if (this.detailsTarget.classList.contains("hidden")) {
+      this.showDetails();
+    } else {
+      this.hideDetails();
+    }
+  }
+
+  showDetails() {
+    this.detailsTarget.classList.remove("hidden");
+    this.buttonTarget.innerText = "Masquer les détails";
+  }
+
+  hideDetails() {
+    this.detailsTarget.classList.add("hidden");
+    this.buttonTarget.innerText = "Afficher les détails";
   }
 }
