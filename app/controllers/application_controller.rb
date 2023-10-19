7a2d58cb136be93ee4649da5dab07da711ff085c
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-10-19 15:34:50 +0200

    devise routes setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,22 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   def after_sign_in_path_for(resource)
-    panier_path
+    if user_signed_in? && current_user.admin?
+      admin_path
+    elsif user_signed_in? && current_order.order_items.present?
+      panier_path
+    else
+      creations_path
+    end
   end
 
   def after_sign_up_path_for(resource)
-    panier_path
+    if user_signed_in? && current_user.admin?
+      admin_path
+    elsif user_signed_in? && current_order.order_items.present?
+      panier_path
+    else
+      creations_path
+    end
   end
 end
