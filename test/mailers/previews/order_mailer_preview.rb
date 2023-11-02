7e55995689360f9d4230cf94051adcc56c6ebf44
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-11-02 13:19:25 +0100

    mailer

--- test/mailers/previews/order_mailer_preview.rb
+++ test/mailers/previews/order_mailer_preview.rb
@@ -2,7 +2,7 @@
 class OrderMailerPreview < ActionMailer::Preview
   def new_order_email
     # Set up a temporary order for the preview
-    order = Order.new(user: User.last, delivery_address: "1-2-3 Chuo, Tokyo, 333-0000", phone: "090-7777-8888")
+    order = Order.first
     OrderMailer.with(order: order).new_order_email
   end
 end
