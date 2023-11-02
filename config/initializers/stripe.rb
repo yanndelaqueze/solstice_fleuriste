48e4cd865ab3cdacb6c31e75eb2545e57bc914ba
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-11-02 14:00:45 +0100

    mailer

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -3,6 +3,7 @@ class StripeCheckoutSessionService
     order = Order.find_by(checkout_session_id: event.data.object.id)
     order.update(status: 'Payée')
     OrderMailer.with(order: @order).new_order_email.deliver_later
+    OrderMailer.with(order: @order).order_confirmation_email.deliver_later
   end
 end
 
