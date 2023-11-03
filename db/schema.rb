5dd8600ca8df41ad47dfa70b9cfb4c74398e4dee
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-11-03 09:00:52 +0100

    implementing logic

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_02_155515) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_03_070709) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -82,6 +82,9 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_02_155515) do
     t.integer "subtotal_cents", default: 0, null: false
     t.integer "total_cents", default: 0, null: false
     t.string "checkout_session_id"
+    t.string "first_name", default: ""
+    t.string "last_name", default: ""
+    t.string "email", default: ""
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
