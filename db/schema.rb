970cc35189eac46f604f3fabfaec2888479f1286
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-11-06 06:40:19 +0100

    prod type

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_05_205739) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_06_053615) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,7 +48,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_05_205739) do
     t.boolean "display", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "product_type", default: "Custom"
   end
 
   create_table "order_items", force: :cascade do |t|
@@ -107,6 +106,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_05_205739) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "min_price_cents", default: 0, null: false
+    t.string "product_type", default: "Custom"
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
