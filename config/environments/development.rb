46f2fa7ba552d0724b443a89edc28d8024bd7f02
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-11-08 16:27:12 +0100

    admin revamp

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -77,14 +77,15 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
 
+
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.default_url_options = { :host => 'localhost:3000', protocol: 'http' }
 config.action_mailer.smtp_settings = {
- address:               "ssl0.ovh.net",
+ address:               'smtp.gmail.com',
  port:                  587,
- domain:                "solsticefleurs.com",
- user_name:             "laurie@solsticefleurs.com",
- password:              "Solstice@2020grogu",
- authentication:        :login,
- enable_starttls_auto:  true}
+ user_name:             ENV["ADMIN_EMAIL"],
+ password:              ENV["GMAIL_PASSWORD"],
+ authentication:        'plain',
+ enable_starttls_auto:  true
+}
 end
