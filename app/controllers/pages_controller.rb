a7f00385a25c1053a65cefc3e443551f63d641f4
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-11-10 10:59:49 +0100

    routes and menu links

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :delivery, :cgv, :mentions_legales, :boutique, :confirmation ]
+  skip_before_action :authenticate_user!, only: [ :home, :delivery, :cgv, :mentions_legales, :boutique, :confirmation, :mariage, :deuil ]
 
   def home
   end
