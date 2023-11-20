e49178c8fcfdc42845111ec0bd98534a56d1e0c8
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-11-20 16:46:38 +0100

    character counter

--- app/javascript/controllers/character_limit_controller.js
+++ app/javascript/controllers/character_limit_controller.js
@@ -1,7 +1,19 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="character-limit"
 export default class extends Controller {
+  static targets = ["input", "counter"];
+
   connect() {
+    console.log("hello character check");
+  }
+
+  checkCharacters() {
+    const currentLength = this.inputTarget.value.length;
+    const maxLength = this.inputTarget.getAttribute("maxlength");
+    this.counterTarget.innerText = `${maxLength} caractères max. (${currentLength}/${maxLength})`;
+    if (currentLength > maxLength) {
+      this.inputTarget.value = this.inputTarget.value.slice(0, maxLength);
+    }
   }
 }
